f0fcaa2feda9490f4870d453567fc5e50ef35ce0
Swilder-M <i@codming.com>
2025-11-20 11:00:14 +0800

    Add v1.80.0.dev6
    Signed-off-by: Swilder-M <i@codming.com>

--- litellm/sync.sh
+++ litellm/sync.sh
@@ -2,7 +2,7 @@
 
 # 定义源目录和目标目录
 SOURCE_DIR=~/Project/litellm-upstream
-TARGET_DIR=~/Project/ai-images/litellm/v1.80.0.rc.1
+TARGET_DIR=~/Project/ai-images/litellm/v1.80.0.dev6
 
 # 展开波浪号
 SOURCE_DIR="${SOURCE_DIR/#\~/$HOME}"
