5076dc35cc8669a5f6d74997f39183a5548e18a0
Swilder-M <i@codming.com>
2026-01-06 16:05:01 +0800

    Add v0.10.5
    Signed-off-by: Swilder-M <i@codming.com>

--- new-api/sync.sh
+++ new-api/sync.sh
@@ -2,7 +2,7 @@
 
 # 定义源目录和目标目录
 SOURCE_DIR=~/Project/new-api-upstream
-TARGET_DIR=~/Project/ai-images/new-api/v0.10.5-ionet.2
+TARGET_DIR=~/Project/ai-images/new-api/v0.10.5
 
 # 展开波浪号
 SOURCE_DIR="${SOURCE_DIR/#\~/$HOME}"
